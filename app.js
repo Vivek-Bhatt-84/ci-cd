f6262e66e73afb8f11e5ee52e91b32181fd0da77
Vivek Bhatt <the9416legend@gmail.com>
2024-05-03 11:51:38 +0530

    added app.js

--- app.js
+++ app.js
@@ -2,3 +2,15 @@ console.log("app.js file for node ");
 console.log(new Date());
 
 console.log("edited in browser");
+const express = require('express');
+const app = express();
+
+const port = "7000";
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Server running' });
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+})
